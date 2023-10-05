37ce0ad2338e2b5b5c9ac38d2f5ce11ec1257b01
Timothy Lam <timothylam1228@gmail.com>
2023-10-05 14:40:42 -0400

    update home page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,16 @@
 import Container from "./Container";
 const Project = ({ project }) => {
+
+    const projects = [{
+        name: "0xblanc",
+    }]
     return (
         <Container>
             <div id="projects" className='group text-black transition-all duration-300 ease-in-out'>
                 <h2 className=' font-extrabold text-5xl bg-left-bottom bg-gradient-to-r from-black to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>Projects</h2>
             </div>
             <div>
-                Coming Soon
+                <div></div>
             </div>
         </Container>
     )
