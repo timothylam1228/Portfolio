f2cf31797d730dc9b3c47eb6d7b952648b1c9178
Timothy Lam <timothylam1228@gmail.com>
2023-11-26 20:52:35 -0500

    responsive design

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -20,9 +20,9 @@ const Project = ({ project }) => {
             <div id="projects" className='group text-black transition-all duration-300 ease-in-out w-full'>
                 <h2 className=' font-extrabold text-5xl bg-left-bottom bg-gradient-to-r from-black to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>Projects</h2>
             </div>
-            <div className="mt-20 flex flex-row gap-4">
+            <div className="mt-20 flex flex-col md:flex-row gap-4 ">
                 {projects.map((project, index) => (
-                    <div className="backdrop-blur-lg aspect-square border-2 border-black md:p-2 w-full sm:w-80 rounded-md relative">
+                    <div className="backdrop-blur-lg aspect-square border-2 border-black md:p-2 w-full sm:w-80 rounded-md relative p-2">
                         <img src={project.preview} alt={project.name} className=" rounded-md" />
                         <a href="https://0xblanc.io/" target="_blank" rel="noreferrer" className="flex text-lg justify-end font-bold font-mono">{project.name}</a>
                         <div className="flex flex-row mt-2 gap-2 flex-wrap items-end justify-end">
